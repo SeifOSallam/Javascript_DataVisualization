271367077b4ec3416e2522d55ee14134f6a2f254
SeifOSallam <seif.osallam@gmail.com>
2023-12-24 19:54:18 +0200

    added fixElements function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var inputElement = document.getElementById("input");
 var searchElement = document.getElementById("search");
 var arrayContainer = document.getElementById("arraycontainer");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 inputElement.addEventListener("keyup", (key) => {
     if (key.code == "Enter") {
         var input = inputElement.value;
@@ -22,20 +25,59 @@ inputElement.addEventListener("keyup", (key) => {
 });
 searchElement.addEventListener("keyup", (key) => {
     if (key.code == "Enter") {
-        var input = inputElement.value;
+        var input = searchElement.value;
         var val = parseInt(input);
         var dataarr = input.split(" ");
         binarySearch(val);
     }
 });
 
-function binarySearch(value) {
+async function binarySearch(value) {
     var dataCollection = document.getElementsByClassName("data");
     var arrayValues = [];
     for (var i=0;i<dataCollection.length;i++) {
         arrayValues[i] = parseInt(dataCollection[i].textContent);
     }
-    arrayValues.forEach((data) => {
-        console.log(data);
-    });
+    var dataContainer = document.getElementsByClassName("datacontainer");
+    fixElements(dataContainer, 0, dataContainer.length);
+    var leftindex = 0;
+    var rightindex = arrayValues.length-1;
+    while (leftindex <= rightindex) {
+        var midindex = Math.ceil(((leftindex+rightindex)/2));
+        console.log("Mid value: " + arrayValues[midindex]);
+        console.log("Mid index: " + midindex);
+        if (value > arrayValues[midindex]) {
+            leftindex = midindex+1;
+        }
+        else if (value < arrayValues[midindex]) {
+            rightindex = midindex-1;
+        }
+        else {
+            console.log("WE DONE");
+            fixElements(dataContainer, leftindex, rightindex);
+            return;
+        }
+        fixElements(dataContainer, leftindex, rightindex);
+        const secondsTimer = async (seconds) => {
+            let sec = seconds;
+            let i = 0;
+            while (i <= sec) {
+                console.log(i);
+                await sleep(1000);
+                i++;
+            }
+        };
+        
+        secondsTimer();
+    }
+}
+function fixElements(elements, startindex, endindex) {
+    for (var i=0;i<elements.length;i++) {
+        if (i >= startindex && i <= endindex) {
+            elements[i].style.backgroundColor = 'green';
+        }
+        else {
+            elements[i].style.backgroundColor = 'red';
+        }
+    }
 }
